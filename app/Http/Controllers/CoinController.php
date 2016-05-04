b243862d93872e76cc15b69db90d2cfa27978801
Angel <info@angelfrancisco.es>
2016-05-05 01:06:38 +0200

    Disabled authentication. Update coin working

--- app/Http/Controllers/CoinController.php
+++ app/Http/Controllers/CoinController.php
@@ -30,14 +30,54 @@ class CoinController extends Controller
 	public function show($id)
 	{
 		//Find coin by id
-		$coin=Fabricante::find($id);
+		$coin=Coin::find($id);
  
 		//If not exists throw error
 		if (!$coin)
 		{
-			return response()->json(['status' => 1, 'message' => 'Recurso no encontrado'],404);
+			return response()->json(['status' => 1, 'message' => 'Recurso no encontrado'], 404);
 		}
  
-		return response()->json(['status' => 0,'result' => $coin],200);
+		return response()->json(['status' => 0,'result' => $coin], 200);
+	}
+	
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update(Request $request, $coinId)
+	{
+		//Check if coin exists
+		$coin=Coin::find($coinId);
+ 
+		//If coin not exists throw error
+		if (!$coin)
+		{
+			return response()->json(['status' => 1, 'message' => 'La moneda no existe'], 404);
+		}	
+ 
+		//Value of status received
+		$status = $request->input('status');
+
+		//Check if method is PUT or PATCH
+		if ($request->method()==='PUT' || $request->method()==='PATCH')
+		{
+			//If status has value, modify it
+			if ($status!=null && $status!='')
+			{
+				$coin->status = $status;
+				
+				//Update value and return
+				$coin->save();
+				return response()->json(['status' => 0,'result' => true], 200);
+			}
+			else
+			{
+				return response()->json(['status' => 1, 'message' => 'No se ha modificado ningún dato'], 404);
+			}
+ 
+		}
 	}
 }
